c1321335b30bf2c5f1bc60491a6e44e75ba8e392
lengmowuya <3537168410@qq.com>
2022-04-12 00:15:15 +0800

    主界面

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
-
+import {NavLink,Route,Routes,Redirect} from 'react-router-dom'
+// import Header from './components/Header/index'
+// import List from './components/List/index'
+import Home from './pages/Home/index'
+import Life from './pages/Life/index'
+import './App.css'
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div id="AppCenter">
+      <Routes id=''>
+        {/* <Header></Header> */}
+        {/* <List></List> */}
+        {/* <Redirect to='/home' /> */}
+        <Route path='/' element={<Home />} exact />
+        <Route path='/home' element={<Home />} />
+        <Route path='/life' element={<Life />}/>
+      </Routes>
+      </div>
     )
   }
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom'
+import './index.css'
+export default class Home extends Component {
+  render() {
+    return (
+      <div id="HomePage">
+          {/* home */}
+          <button className='startGame'>
+            <NavLink to="/life">开始人生！</NavLink>
+          </button>
+      </div>
+    )
+  }
+}
