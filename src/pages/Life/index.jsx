70e2c30e69b1733b81cbca03555f5a824cb0694c
lengmowuya <3537168410@qq.com>
2022-04-15 22:18:46 +0800

    搭建简易词库和年龄分段

--- src/pages/Life/index.jsx
+++ src/pages/Life/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import classNames from 'classnames'
-import cityLibrary from './../../library/area/city.json'
+import city from './../../library/area/city.json'
 import distance from './../../library/area/distance.json'
 import rural from './../../library/area/rural.json'
 import './index.css'
@@ -13,6 +13,7 @@ export default class Life extends Component {
             sex:'男',
             area:'农村',
             name:'张建安',
+            areaLibrary:null
         },
         famaily:[
             {identity:"父亲",industry:"赤脚医生"},
@@ -30,19 +31,19 @@ export default class Life extends Component {
         lifeStorys:[
         ],
         entryLibrary:[
-            '你总是遭遇校园霸凌',
+            // '你总是遭遇校园霸凌',
             '你换了好几颗牙',
             '你生了场重病，家里花了不少钱',
-            '到同学家借书看',
-            '上山偷橘子差点被主人发现',
-            '你追得家里的动物鸡飞狗跳',
-            '你偷了父母的零钱去买零食',
-            '今年股市大崩，股民损失惨重',
+            // '到同学家借书看',
+            '偷橘子差点被主人家发现',
+            // '你追得家里的动物鸡飞狗跳',
+            // '你偷了父母的零钱去买零食',
+            // '今年股市大崩，股民损失惨重',
             '顺利的一年，没发生什么大事',
-            '你的身体越来越差了',
+            // '你的身体越来越差了',
             '你对人生有些迷茫，还有些懵懂',
-            '你小病不断',
-            '你发觉生活越来越无趣'
+            '你得了感冒',
+            '你发觉生活有些时候很无趣'
         ],
         entryList:[]
     }
@@ -55,17 +56,40 @@ export default class Life extends Component {
         const randomArea = Math.random();
         if(randomArea <= 0.15){
             role.area = '偏远地区'
+            role.areaLibrary = distance
         }else if(randomArea <= 0.65){
             role.area = '农村'
+            role.areaLibrary = rural
         }else{
             role.area = '城市'
+            role.areaLibrary = city
         }
         this.setState({role})
     }
+    getRandomAreaEntry =()=>{
+        const {role} = this.state
+        const {age,areaLibrary} = role;
+        let targetLibrary;
+        if(age<18){
+            targetLibrary = areaLibrary.baseEntrys.childhood;
+        }else{
+            targetLibrary = areaLibrary.baseEntrys.adult;
+        }
+        // 如果目标词库为空
+        if(targetLibrary.length<=0) return '今年无事发生'
+        const randomNumber = Math.floor(Math.random()*targetLibrary.length);
+        return targetLibrary[randomNumber];
+    }
     getRandomEntry = ()=>{
         const {entryLibrary} = this.state
-        const randomNumber = Math.floor(Math.random()*entryLibrary.length);
-        return entryLibrary[randomNumber];
+        let entry;
+        if(Math.random()<= 0.2){
+            const randomNumber = Math.floor(Math.random()*entryLibrary.length);
+            entry = entryLibrary[randomNumber];
+        }else{
+            entry = this.getRandomAreaEntry();
+        }
+        return entry;
     }
     nextStory = ()=>{
         const {lifeStorys,role} = this.state
